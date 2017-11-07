33590d5e7291b4a995d93f5c18db8c39f13ebd06
Aaron <zcababl@ucl.ac.uk>
2017-11-07 07:15:21 +0000

    Updated RenderableComponent
    For some reason, I forgot to commit the renderable component change.

--- src/aaron/game/pokemonatb/component/RenderableComponent.java
+++ src/aaron/game/pokemonatb/component/RenderableComponent.java
@@ -2,5 +2,10 @@ package aaron.game.pokemonatb.component;
 
 public class RenderableComponent extends Component {
 	public boolean active = true;
-	public RenderableComponent(){}
+	public int x = 0;
+	public int y = 0;
+	public int layer = 0;
+	public RenderableComponent(int layer){
+		this.layer = layer;
+	}
 }
